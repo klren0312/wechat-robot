abfeb2b3456600d85c940b540a6cefc578ab56df
zzes <kuanglong0312@sina.com>
2019-01-31 11:25:44 +0800

    添加智能回复

--- examples/manyUserRobot.js
+++ examples/manyUserRobot.js
@@ -1,6 +1,10 @@
 const { Wechaty } = require('wechaty')
 const schedule = require('node-schedule')
-const contactArr = require('./contactArr').contactArr
+const fly = require('flyio')
+
+const contactArr = require('./robot-config').contactArr
+const personArr = require('./robot-config').personArr
+
 // 定义
 const bot = new Wechaty({
   name: 'zzes-wechat-bot'
@@ -24,8 +28,10 @@ bot
   })
 
 // 事件定义
-function onScan (qrcode, status) {
-  require('qrcode-terminal').generate(qrcode, { small: true })  // show qrcode on console
+function onScan(qrcode, status) {
+  require('qrcode-terminal').generate(qrcode, {
+    small: true
+  }) // show qrcode on console
   const qrcodeImageUrl = [
     'https://api.qrserver.com/v1/create-qr-code/?data=',
     encodeURIComponent(qrcode),
@@ -35,40 +41,80 @@ function onScan (qrcode, status) {
 }
 
 const timer = null
-function onLogin (user) {
+
+function onLogin(user) {
   console.log(`${user.name()} 登录成功`)
   bot.say('机器人登录').catch(console.error)
   const date = new Date(2019, 2, 5, 0, 0, 0)
-  timer = schedule.scheduleJob(date, function() {
+  timer = schedule.scheduleJob(date, function () {
     main()
   })
 }
 
-function onLogout (user) {
+function onLogout(user) {
   console.log(`${user.name()} 退出登录`)
 }
 
-function onError (e) {
+function onError(e) {
   console.error('机器人报错:', e)
 }
 
 // get msg
-async function onMessage (msg) {
+async function onMessage(msg) {
   console.log(msg.toString())
+  const contact = msg.from()
+  const text = msg.text()
+  const room = msg.room()
+  /**
+   * 拦截以下信息:
+   * (x) 群聊
+   * (x) 自己信息
+   * (x) 公众号信息 contact.type() 为 1 则是公众号
+   * (x) 白名单信息
+   */
+  if (!room && !msg.self() && personArr.indexOf(contact.name()) === -1 && contact.type() === 1) {
+    console.log(`${contact.name()}  给你发送了   ${text}`)
+    let gender = ''
+    if (contact.gender() === 1) {
+      gender = '先生'
+    } else {
+      gender = '女士'
+    }
+    fly.post(`http://api.ruyi.ai/v1/message`, {
+      q: text,
+      app_key: '',
+      user_id: contact.name()
+    }).then(async res => {
+      console.log(res.data.result)
+      const r = res.data
+      if (r.code === 0) {
+        if (r.result.hasOwnProperty('intents')) {
+          await msg.say(r.result.intents[0].result.text)
+        } else {
+          return
+        }
+      } else {
+        await msg.say(`${gender}您好, 暂时无法理解您发送的内容, 祝您春节快乐!`)
+      }
+
+    })
+  }
 }
 
 /**
  * Main Contact Bot
  */
 async function main() {
-  setTimeout(function(){
+  setTimeout(function () {
     contactArr.forEach(async (v) => {
-      const contact =  await bot.Contact.find({alias: v})
-      await contact.say(`春节快乐! ${v} 新的一年, 继续加油!`)
+      const contact = await bot.Contact.find({
+        alias: v
+      })
+      await contact.say(`春节快乐! 新的一年, ${v}继续加油!`)
     })
     timer && timer.cancel()
-  },6000);
-  
+  }, 6000);
+
 }
 /**
  *
---
+++ examples/robot-config.js
@@ -0,0 +1,11 @@
+// 群发名单
+const contactArr = [
+  '自己维护的群发名单'
+]
+exports.contactArr = contactArr
+
+// 白名单
+const personArr = [
+  '自己维护的自动回复白名单'
+]
+exports.personArr = personArr
